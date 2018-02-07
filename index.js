8878d84ae43316bc3cd3009137c73a9f6617f679
Duane Pearson <dp.anarchyink@gmail.com>
2018-02-07 13:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (name=== "Susan") { console.log ("The name is correct")
 } else {
   console.log ("Expected the name to be Suasan")
 }
-if (height=== "70") { console.log ("The height is correct")
+if (height=== 70) { console.log ("The height is correct")
 } else {
   console.log ("Expected the height to be 70")
 }
